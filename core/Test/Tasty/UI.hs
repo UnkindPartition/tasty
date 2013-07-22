ef83232fa5284139b3e0c01f320e35da95cff7b5
Roman Cheplyaka <roma@ro-che.info>
2013-07-23 00:37:58 +0300

    UI: better printing of the test tree

--- core/Test/Tasty/UI.hs
+++ core/Test/Tasty/UI.hs
@@ -9,22 +9,38 @@ import Text.Printf
 import qualified Data.IntMap as IntMap
 import Data.Maybe
 
+data RunnerState = RunnerState
+  { ix :: !Int
+  , nestedLevel :: !Int
+  }
+
+initialState :: RunnerState
+initialState = RunnerState 0 0
+
+type M = StateT RunnerState IO
+
+indentSize :: Int
+indentSize = 2
+
+indent :: Int -> String
+indent n = replicate (indentSize * n) ' '
+
 -- | A simple console UI
 runUI :: Runner
 runUI opts tree smap = do
-  flip evalStateT 0 $ getApp $
+  flip evalStateT initialState $ getApp $
     foldTestTree
       (runSingleTest smap)
-      (const id)
+      runGroup
       opts
       tree
   where
     runSingleTest
       :: IsTest t
       => IntMap.IntMap (TVar Status)
-      -> OptionSet -> TestName -> t -> AppMonoid (StateT Int IO)
+      -> OptionSet -> TestName -> t -> AppMonoid M
     runSingleTest smap _opts name _test = AppMonoid $ do
-      ix <- get
+      st@RunnerState { ix = ix, nestedLevel = level } <- get
       let
         statusVar =
           fromMaybe (error "internal error: index out of bounds") $
@@ -35,7 +51,15 @@ runUI opts tree smap = do
           Done r -> return $ resultSuccessful r
           Exception _ -> return False
           _ -> retry
-      liftIO $ printf "%s: %s\n" name
+      liftIO $ printf "%s%s: %s\n" (indent level) name
         (if ok then "OK" else "FAIL")
       let ix' = ix+1
-      put $! ix'
+      put $! st { ix = ix' }
+
+    runGroup :: TestName -> AppMonoid M -> AppMonoid M
+    runGroup name (AppMonoid act) = AppMonoid $ do
+      st@RunnerState { nestedLevel = level } <- get
+      liftIO $ printf "%s%s\n" (indent level) name
+      put $! st { nestedLevel = level + 1 }
+      act
+      modify $ \st -> st { nestedLevel = level }
