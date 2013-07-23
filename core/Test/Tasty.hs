6ef6c4dc4f5a41c4c581225bdff389630e033ddb
Roman Cheplyaka <roma@ro-che.info>
2013-07-23 18:01:12 +0300

    Add functions for local option adjustments

--- core/Test/Tasty.hs
+++ core/Test/Tasty.hs
@@ -6,14 +6,30 @@ module Test.Tasty
   , testGroup
   , defaultMain
   , defaultMainWithRunner
+  -- * Adjusting options
+  -- | Normally options are specified on the command line. But you can
+  -- also have different options for different subtrees in the same tree,
+  -- using the functions below.
+  , adjustOption
+  , localOption
   )
   where
 
 import Test.Tasty.Core
 import Test.Tasty.Runners
 import Test.Tasty.UI
+import Test.Tasty.Options
 
 -- | Parse the command line arguments and run the tests using the standard
 -- console runner
 defaultMain :: TestTree -> IO ()
 defaultMain = defaultMainWithRunner runUI
+
+-- | Locally adjust the option value for the given test subtree
+adjustOption :: IsOption v => (v -> v) -> TestTree -> TestTree
+adjustOption f = PlusTestOptions $ \opts ->
+  setOption (f $ lookupOption opts) opts
+
+-- | Locally set the option value for the given test subtree
+localOption :: IsOption v => v -> TestTree -> TestTree
+localOption v = PlusTestOptions (setOption v)
