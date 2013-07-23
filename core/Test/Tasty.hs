2a867ba494bf6e65ccb4113dcc5f886310d6fce3
Roman Cheplyaka <roma@ro-che.info>
2013-07-23 09:50:57 +0300

    Make Test.Tasty.UI hidden

--- core/Test/Tasty.hs
+++ core/Test/Tasty.hs
@@ -4,11 +4,18 @@ module Test.Tasty
   ( TestName
   , TestTree
   , testGroup
+  , defaultMain
+  , defaultMainWithRunner
   )
   where
 
 import Test.Tasty.Core
+import Test.Tasty.Runners
+import Test.Tasty.UI
 
 -- | Create a named group of test cases or other groups
 testGroup :: TestName -> [TestTree] -> TestTree
 testGroup = TestGroup
+
+defaultMain :: TestTree -> IO ()
+defaultMain = defaultMainWithRunner runUI
