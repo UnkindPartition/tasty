be133f083490faca3f58e55fde381c5d2596eca0
Roman Cheplyaka <roma@ro-che.info>
2013-07-28 13:54:12 +0300

    Add and document additional exception handling

--- core/Test/Tasty/Parallel.hs
+++ core/Test/Tasty/Parallel.hs
@@ -1,28 +1,40 @@
 -- | A helper module which takes care of parallelism
-module Test.Tasty.Parallel (runInParallel) where
+module Test.Tasty.Parallel (Action(..), runInParallel) where
 
 import Control.Monad
 import Control.Concurrent
 import Control.Concurrent.STM
+import Control.Exception
+
+data Action = Action
+  { action :: IO () -- ^ action to execute
+  , handleExn :: SomeException -> IO () -- ^ exception handler
+  }
 
 -- | Take a list of actions and execute them in parallel, no more than @n@
 -- at the same time
 runInParallel
   :: Int -- ^ maximum number of parallel threads
-  -> [IO ()] -- ^ list of actions to execute
+  -> [Action] -- ^ list of actions to execute
   -> IO ()
 runInParallel nthreads actions = do
   capsVar <- atomically $ newTVar nthreads
 
   let
-    go action cont = join . atomically $ do
+    go a cont = join . atomically $ do
       caps <- readTVar capsVar
       if caps > 0
         then do
           writeTVar capsVar $! caps - 1
           let
             release = atomically $ modifyTVar' capsVar (+1)
-          return $ forkIO (action >> release) >> cont
+
+          return $ do
+            forkFinally (action a) $ \e -> do
+              release
+              either (handleExn a) return e
+            cont
+
         else retry
 
   foldr go (return ()) actions
--- core/Test/Tasty/Run.hs
+++ core/Test/Tasty/Run.hs
@@ -31,7 +31,7 @@ data Status
 
 data TestMap = TestMap
     !Int
-    !(IntMap.IntMap (IO (), TVar Status))
+    !(IntMap.IntMap (Action, TVar Status))
       -- ^ Int is the first free index
       --
       -- IntMap maps test indices to:
@@ -90,6 +90,19 @@ executeTest action statusVar = do
         Right result -> return $ Done result
 
 -- | Prepare the test tree to be run
+--
+-- A note about exceptions. Exceptions are caught in two places.
+-- One is in 'executeTest' (synchonous exceptions only). Another is when
+-- the 'handleExn' component of the action is invoked.
+--
+-- It may seem redundant, but it's not. Exception handling in 'executeTest'
+-- is to make sure that an exception from a test cannot propagate
+-- to the testing program.
+--
+-- Exception handling using 'handleExn' is needed to ensure that every
+-- test eventually finishes. Without it, a (possibly asynchonous)
+-- exception could kill the thread executing a test case, and we would
+-- then deadlock waiting on its status var.
 createTestMap :: OptionSet -> TestTree -> IO TestMap
 createTestMap opts tree =
   flip execStateT (TestMap 0 IntMap.empty) $ getApp $
@@ -101,7 +114,11 @@ createTestMap opts tree =
   where
     runSingleTest opts _ test = AppMonoid $ do
       statusVar <- liftIO $ atomically $ newTVar NotStarted
-      let act = executeTest (run opts test) statusVar
+      let
+        act =
+          Action
+            (executeTest (run opts test) statusVar)
+            (atomically . writeTVar statusVar . Exception)
       TestMap ix tmap <- get
       let
         tmap' = IntMap.insert ix (act, statusVar) tmap
