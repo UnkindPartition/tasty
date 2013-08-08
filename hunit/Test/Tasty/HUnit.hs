d4e07297cd4a36676e642c092859d2f81066cbaf
Roman Cheplyaka <roma@ro-che.info>
2013-08-08 15:46:07 +0300

    tasty-hunit: cleanup

--- hunit/Test/Tasty/HUnit.hs
+++ hunit/Test/Tasty/HUnit.hs
@@ -14,42 +14,23 @@ import Control.Monad.Trans
 testCase :: TestName -> Assertion -> TestTree
 testCase name = singleTest name . TestCase
 
-data TestCaseResult
-  = TestCasePassed
-  | TestCaseFailed String
-  | TestCaseError String
-
-describe r =
-  case r of
-    TestCasePassed         -> ""
-    TestCaseFailed message -> message
-    TestCaseError message  -> message
-
-testCaseSucceeded :: TestCaseResult -> Bool
-testCaseSucceeded TestCasePassed = True
-testCaseSucceeded _              = False
-
 newtype TestCase = TestCase Assertion
     deriving Typeable
 
 instance IsTest TestCase where
-  run _ (TestCase assertion) _ =
-    myPerformTestCase assertion
+  run _ (TestCase assertion) _ = do
+    hunitResult <- performTestCase assertion
+    return $
+      case hunitResult of
+        Nothing ->
+          Result
+            { resultSuccessful = True
+            , resultDescription = ""
+            }
+        Just (_, message)  ->
+          Result
+            { resultSuccessful = False
+            , resultDescription = message
+            }
 
   testOptions = return []
-
-myPerformTestCase :: Assertion -> IO Result
-myPerformTestCase assertion = do
-    hunitResult <- performTestCase assertion
-    let
-      tcResult =
-        case hunitResult of
-          Nothing               -> TestCasePassed
-          Just (True, message)  -> TestCaseFailed message
-          Just (False, message) -> TestCaseError message
-      result =
-        Result
-          { resultSuccessful = testCaseSucceeded tcResult
-          , resultDescription = describe tcResult
-          }
-    return result
