b3832843e4cb20b3cb1ab883166d90b07a51477f
Roman Cheplyaka <roma@ro-che.info>
2014-02-20 19:07:12 +0200

    Add a couple of resource tests and improve existing ones

--- core-tests/Resources.hs
+++ core-tests/Resources.hs
@@ -17,7 +17,19 @@ import Control.Exception
 import Utils
 
 testResources = testGroup "Resources"
-  [testResources1, testResources2]
+  [testResources1, testResources2, testResources3, testResources4]
+
+initIORef ref = do
+  v <- readIORef ref
+  if v
+    then assertFailure "resource was already initialized!"
+    else writeIORef ref True
+  return ref
+releaseIORef ref = do
+  v <- readIORef ref
+  if not v
+    then assertFailure "resource was not initialized!"
+    else writeIORef ref False
 
 ------------------------------
 -- Normal operation
@@ -25,7 +37,7 @@ testResources = testGroup "Resources"
 -- this is a dummy tree we use for testing
 testTree1 :: IORef Bool -> TestTree
 testTree1 ref =
-  withResource (ref <$ writeIORef ref True) (\ref -> writeIORef ref False) $ \ioRef ->
+  withResource (initIORef ref) releaseIORef $ \ioRef ->
   testGroup "dummy"
     [ testCase "aaa" $ check ioRef
     , testCase "bbb" $ check ioRef
@@ -64,3 +76,35 @@ testResources2 = testCase "Exception during resource initialization" $
     Failure (TestThrewException (fromException -> Just (ErrorCall "exInit"))) ->
       return ()
     c -> assertFailure $ "Unexpected outcome: " ++ show c
+
+testTree3 :: IORef Bool -> TestTree
+testTree3 ref =
+  withResource (initIORef ref) releaseIORef $ \ioRef -> testCase "body" $
+    error "exBody"
+
+testResources3 :: TestTree
+testResources3 = testCase "Exception in test body; resource is released" $ do
+  ref <- newIORef False
+  launchTestTree mempty (testTree3 ref) $ \smap abort -> do
+    [r] <- runSMap smap
+    case resultOutcome r of
+      Failure (TestThrewException (fromException -> Just (ErrorCall "exBody"))) ->
+        return ()
+      c -> assertFailure $ "Unexpected outcome: " ++ show c
+    b <- readIORef ref
+    assertBool "Resource wasn't released" (not b)
+
+testTree4 :: IORef Bool -> TestTree
+testTree4 ref =
+  withResource (initIORef ref) (error "exFin") $ \ioRef -> testCase "body" $
+  void ioRef
+
+testResources4 :: TestTree
+testResources4 = testCase "Exception in finalizer" $ do
+  ref <- newIORef False
+  launchTestTree mempty (testTree4 ref) $ \smap abort -> do
+    [r] <- runSMap smap
+    case resultOutcome r of
+      Failure (TestThrewException (fromException -> Just (ErrorCall "exFin"))) ->
+        return ()
+      c -> assertFailure $ "Unexpected outcome: " ++ show c
