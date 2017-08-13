c4be2731eb199eceb23daa54ad33f2230c8bc26c
Roman Cheplyaka <roma@ro-che.info>
2017-08-13 10:47:38 +0300

    Install signal handlers

--- core/Test/Tasty/CmdLine.hs
+++ core/Test/Tasty/CmdLine.hs
@@ -1,5 +1,5 @@
 -- | Parsing options supplied on the command line
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE CPP, ScopedTypeVariables, DeriveDataTypeable #-}
 module Test.Tasty.CmdLine
   ( optionParser
   , suiteOptions
@@ -10,16 +10,27 @@ module Test.Tasty.CmdLine
 import Options.Applicative
 import Data.Monoid
 import Data.Proxy
-import Data.Foldable
+import Data.Foldable (foldMap)
 import Prelude  -- Silence AMP and FTP import warnings
 import System.Exit
 import System.IO
+
+-- for installSignalHandlers
+#ifdef UNIX
+import Control.Concurrent (mkWeakThreadId, myThreadId)
+import Control.Exception (Exception(..), throwTo)
+import Control.Monad (forM_)
+import Data.Typeable (Typeable)
+import System.Posix.Signals
+import System.Mem.Weak (deRefWeak)
+#endif
 
 import Test.Tasty.Core
 import Test.Tasty.Ingredients
 import Test.Tasty.Options
 import Test.Tasty.Options.Env
 import Test.Tasty.Runners.Reducers
+
 
 -- | Generate a command line parser from a list of option descriptions
 optionParser :: [OptionDescription] -> Parser OptionSet
@@ -40,6 +51,7 @@ suiteOptionParser ins tree = optionParser $ suiteOptions ins tree
 -- details.
 defaultMainWithIngredients :: [Ingredient] -> TestTree -> IO ()
 defaultMainWithIngredients ins testTree = do
+  installSignalHandlers
   cmdlineOpts <- execParser $
     info (helper <*> suiteOptionParser ins testTree)
     ( fullDesc <>
@@ -58,3 +70,28 @@ defaultMainWithIngredients ins testTree = do
     Just act -> do
       ok <- act
       if ok then exitSuccess else exitFailure
+
+-- from https://ro-che.info/articles/2014-07-30-bracket
+-- Install a signal handler so that e.g. the cursor is restored if the test
+-- suite is killed by SIGTERM.
+installSignalHandlers :: IO ()
+installSignalHandlers = do
+#ifdef UNIX
+  main_thread_id <- myThreadId
+  weak_tid <- mkWeakThreadId main_thread_id
+  forM_ [ sigABRT, sigBUS, sigFPE, sigHUP, sigILL, sigQUIT, sigSEGV,
+          sigSYS, sigTERM, sigUSR1, sigUSR2, sigXCPU, sigXFSZ ] $ \sig ->
+    installHandler sig (Catch $ send_exception weak_tid sig) Nothing
+  where
+    send_exception weak_tid sig = do
+      m <- deRefWeak weak_tid
+      case m of
+        Nothing  -> return ()
+        Just tid -> throwTo tid (toException $ SignalException sig)
+
+newtype SignalException = SignalException Signal
+  deriving (Show, Typeable)
+instance Exception SignalException
+#else
+  return ()
+#endif
