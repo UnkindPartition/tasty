b084be749763b589749a04ff4995a9004e70ed8f
Roman Cheplyaka <roma@ro-che.info>
2017-08-13 11:58:07 +0300

    Add exit status tests for the --quiet mode

--- core-tests/exit-status-test.hs
+++ core-tests/exit-status-test.hs
@@ -1,17 +1,44 @@
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveDataTypeable, BangPatterns #-}
 import Test.Tasty
 import Test.Tasty.Options
 import Test.Tasty.HUnit
 import Data.Proxy
 import Data.Typeable
 import Text.Read
+import Control.Concurrent
+import System.Random (newStdGen, RandomGen)
+import System.Random.Shuffle (shuffle')
+
+-- The shuffle' function from System.Random.Shuffle is broken; it doesn't work on empty lists.
+shuffle :: RandomGen gen => [a] -> Int -> gen -> [a]
+shuffle l n g
+  | n == 0 = []
+  | otherwise = shuffle' l n g
 
 main :: IO ()
-main = defaultMainWithIngredients
-  (includingOptions [Option (Proxy :: Proxy Result)] : defaultIngredients) $
+main = do
+  gen <- newStdGen
+
+  defaultMainWithIngredients (includingOptions allOptions : defaultIngredients) $
     askOption $ \(Result requestedResult) ->
-      testCase "Test" $
-        assertBool "Requested failure" requestedResult
+    askOption $ \(Fast n_fast) ->
+    askOption $ \(Slow n_slow) ->
+      testGroup "Test" $
+        -- we shuffle fast and slow tests to test various code paths in the 'statusMapResult' function
+        shuffle
+          (replicate n_slow (testCase "Slow test" $ threadDelay (2*10^6)) ++
+           replicate n_fast (testCase "Fast test" $ threadDelay (10^4)))
+          (n_slow + n_fast) -- number of elements
+          gen
+        ++
+        [ testCase "The test that may fail" $ assertBool "Requested failure" requestedResult ]
+
+allOptions :: [OptionDescription]
+allOptions =
+  [ Option (Proxy :: Proxy Result)
+  , Option (Proxy :: Proxy Slow)
+  , Option (Proxy :: Proxy Fast)
+  ]
 
 newtype Result = Result Bool
   deriving Typeable
@@ -20,3 +47,17 @@ instance IsOption Result where
   parseValue = fmap Result . readMaybe
   optionName = return "result"
   optionHelp = return "True means the suite will succeed, False means it will fail"
+newtype Fast = Fast Int
+  deriving Typeable
+instance IsOption Fast where
+  defaultValue = Fast 0
+  parseValue = fmap Fast . readMaybe
+  optionName = return "fast"
+  optionHelp = return "Number of fast tests to add"
+newtype Slow = Slow Int
+  deriving Typeable
+instance IsOption Slow where
+  defaultValue = Slow 0
+  parseValue = fmap Slow . readMaybe
+  optionName = return "slow"
+  optionHelp = return "Number of slow tests to add"
--- core-tests/exit-status-tests.sh
+++ core-tests/exit-status-tests.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 
-set -eux
+set -ux
 
 # This assumes that the compiled exit-status-test executable
 # is in the PATH
 
-exit-status-test --result=True > /dev/null
-exit-status-test --result=True --quiet
+exit-status-test --result=True > /dev/null || exit 1
+exit-status-test --result=True --quiet || exit 1
 
-! exit-status-test --result=False > /dev/null
-! exit-status-test --result=False --quiet
+! exit-status-test --result=False > /dev/null || exit 1
+! exit-status-test --result=False --quiet || exit 1
+
+# Testing the --quiet mode
+# We try different number of fast tests in order to test different paths in the 'statusMapResult' function.
+# The number of fast tests should not affect anything.
+
+for fast in `seq 0 20`; do
+  # When the number of slow tests < number of threads, should find the failing
+  # test and short-circuit
+  timeout 1 exit-status-test --result=False --slow=3 --fast=$fast --num-threads=4 --quiet
+  [ $? -eq 1 ] || exit 1
+  # When the number of slow tests >= number of threads, cannot find the failing
+  # test
+  timeout 1 exit-status-test --result=False --slow=4 --num-threads=4 --quiet
+  [ $? -eq 124 ] || exit 1
+  # Successful case (and no slow tests)
+  timeout 1 exit-status-test --result=True --fast=$fast --num-threads=4 --quiet || exit 1
+done
