e047706b3050afceb11595bf23c307d430f8d242
Roman Cheplyaka <roma@ro-che.info>
2018-01-04 23:53:05 +0200

    Add a minimal example to the haddock
    Fix #160.

--- core/Test/Tasty.hs
+++ core/Test/Tasty.hs
@@ -1,5 +1,30 @@
 -- | This module defines the main data types and functions needed to use
 -- Tasty.
+--
+-- To create a test suite, you also need one or more test providers, such
+-- as
+-- <https://hackage.haskell.org/package/tasty-hunit tasty-hunit> or
+-- <https://hackage.haskell.org/package/tasty-quickcheck tasty-quickcheck>.
+--
+-- A simple example (using tasty-hunit) is
+--
+-- >import Test.Tasty
+-- >import Test.Tasty.HUnit
+-- >
+-- >main = defaultMain tests
+-- >
+-- >tests :: TestTree
+-- >tests = testGroup "Tests"
+-- >  [ testCase "2+2=4" $
+-- >      2+2 @?= 4
+-- >  , testCase "7 is even" $
+-- >      assertBool "Oops, 7 is odd" (even 7)
+-- >  ]
+--
+-- Take a look at the <https://github.com/feuerbach/tasty#readme README>:
+-- it contains a comprehensive list of test providers, a bigger example,
+-- and a lot of other information.
+
 module Test.Tasty
   (
   -- * Organizing tests
