7bd36ca6653e0282c590429cdbfc1eaafd977ce5
Roman Cheplyaka <roma@ro-che.info>
2018-06-23 14:37:13 +0100

    Raise the timeout to 1.5s in an attempt to avoid random test failures

--- core-tests/exit-status-tests.sh
+++ core-tests/exit-status-tests.sh
@@ -15,15 +15,17 @@ exit-status-test --result=True --quiet || exit 1
 # We try different number of fast tests in order to test different paths in the 'statusMapResult' function.
 # The number of fast tests should not affect anything.
 
+TIMEOUT=1.5 # seconds
+
 for fast in `seq 0 20`; do
   # When the number of slow tests < number of threads, should find the failing
   # test and short-circuit
-  timeout 1 exit-status-test --result=False --slow=3 --fast=$fast --num-threads=4 --quiet
+  timeout "$TIMEOUT" exit-status-test --result=False --slow=3 --fast=$fast --num-threads=4 --quiet
   [ $? -eq 1 ] || exit 1
   # When the number of slow tests >= number of threads, cannot find the failing
   # test
-  timeout 1 exit-status-test --result=False --slow=4 --num-threads=4 --quiet
+  timeout "$TIMEOUT" exit-status-test --result=False --slow=4 --num-threads=4 --quiet
   [ $? -eq 124 ] || exit 1
   # Successful case (and no slow tests)
-  timeout 1 exit-status-test --result=True --fast=$fast --num-threads=4 --quiet || exit 1
+  timeout "$TIMEOUT" exit-status-test --result=True --fast=$fast --num-threads=4 --quiet || exit 1
 done
