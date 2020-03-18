2334ce1ead271f446e8bb0c30ff9422f33ee31b9
Ryan Scott <ryan.gl.scott@gmail.com>
2020-03-18 17:05:04 -0400

    Use value/showDefaultWith instead of manually displaying "default: <value>"
    This follows up on
    https://github.com/feuerbach/tasty-golden/pull/33#issuecomment-596046594
    by using `optparse-applicative`'s `value` and `showDefaultWith`
    combinators to display default values in `--help` output instead of
    hard-wiring the phrase `default: <value>` into the help description.

--- core/Test/Tasty/Options/Core.hs
+++ core/Test/Tasty/Options/Core.hs
@@ -35,8 +35,12 @@ instance IsOption NumThreads where
   defaultValue = NumThreads numCapabilities
   parseValue = mfilter onlyPositive . fmap NumThreads . safeRead
   optionName = return "num-threads"
-  optionHelp = return "Number of threads to use for tests execution (default: # of cores/capabilities)"
-  optionCLParser = mkOptionCLParser (short 'j' <> metavar "NUMBER")
+  optionHelp = return "Number of threads to use for tests execution"
+  optionCLParser = mkOptionCLParser $
+    short 'j' <>
+    metavar "NUMBER" <>
+    value defaultValue <>
+    showDefaultWith (\_ -> "# of cores/capabilities")
 
 -- | Filtering function to prevent non-positive number of threads
 onlyPositive :: NumThreads -> Bool
