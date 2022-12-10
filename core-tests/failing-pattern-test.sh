6ac8b87829c0a6a9fd83d2de82d146976a66f6bd
Bodigrim <andrew.lelechenko@gmail.com>
2022-12-10 22:52:34 +0000

    Shellcheck test scripts

--- core-tests/failing-pattern-test.sh
+++ core-tests/failing-pattern-test.sh
@@ -5,7 +5,7 @@ set -eu
 # Make sure the loop is interrupted by Ctrl-C
 trap exit INT
 
-for seed in `seq 1 10`; do
+for seed in $(seq 1 10); do
   failing-pattern-test --seed "$seed" | perl -lne '/Use (.*) to rerun this test only\./ && print $1' |
     { n_patterns=0
       while read -r pattern; do
