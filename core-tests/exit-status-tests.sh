d6aa787025f0d600207c27347b5718c85b1d1e11
Bodigrim <andrew.lelechenko@gmail.com>
2022-12-10 22:52:34 +0000

    Shellcheck test scripts

--- core-tests/exit-status-tests.sh
+++ core-tests/exit-status-tests.sh
@@ -17,15 +17,15 @@ exit-status-test --result=True --quiet || exit 1
 
 TIMEOUT=3 # seconds
 
-for fast in `seq 0 20`; do
+for fast in $(seq 0 20); do
   # When the number of slow tests < number of threads, should find the failing
   # test and short-circuit
-  timeout "$TIMEOUT" exit-status-test --result=False --slow=3 --fast=$fast --num-threads=4 --quiet
+  timeout "$TIMEOUT" exit-status-test --result=False --slow=3 --fast="$fast" --num-threads=4 --quiet
   [ $? -eq 1 ] || exit 1
   # When the number of slow tests >= number of threads, cannot find the failing
   # test
   timeout "$TIMEOUT" exit-status-test --result=False --slow=4 --num-threads=4 --quiet
   [ $? -eq 124 ] || exit 1
   # Successful case (and no slow tests)
-  timeout "$TIMEOUT" exit-status-test --result=True --fast=$fast --num-threads=4 --quiet || exit 1
+  timeout "$TIMEOUT" exit-status-test --result=True --fast="$fast" --num-threads=4 --quiet || exit 1
 done
